0fe3d5e3cf8de877b270238baafa57c670593e70
ItsMeViipeR <itsmeviiper@gmail.com>
2026-04-20 21:36:58 +0200

    fix: add omitempty and omitzero

--- api/types/message.go
+++ api/types/message.go
@@ -3,10 +3,10 @@ package types
 import "time"
 
 type Message struct {
-	ID        int64     `json:"id"`
+	ID        int64     `json:"id,omitempty"`
 	Content   string    `json:"content"`
 	UserID    int64     `json:"user_id"`
 	User      *User     `json:"user,omitempty"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"created_at,omitzero"`
 	TopicID   int64     `json:"topic_id"`
 }
