da24e0fcca2b33b3943203ee1b662c23e456a1bd
ItsMeViipeR <itsmeviiper@gmail.com>
2026-04-20 13:51:51 +0200

    fix: return true when token is expired

--- frontend/src/lib/isTokenExpired.ts
+++ frontend/src/lib/isTokenExpired.ts
@@ -7,7 +7,7 @@ export const isTokenExpired = (token: string): boolean => {
 		const currentTime = Date.now() / 1000;
 
 		if (decoded.exp && decoded.exp > currentTime) {
-			return false;
+			return true;
 		}
 
 		return false;
