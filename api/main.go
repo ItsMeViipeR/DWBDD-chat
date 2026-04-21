082d52ffe7265bfeaf5d970f2a7e5c854df45a82
ItsMeViipeR <itsmeviiper@gmail.com>
2026-04-21 18:04:56 +0200

    feat: add LoginHandler

--- api/main.go
+++ api/main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/ItsMeViipeR/DWBDD-chat/api/types"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt"
 	"github.com/supabase-community/postgrest-go"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -33,6 +31,8 @@ func main() {
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
+
+	r.POST("/api/login", LoginHandler)
 
 	authGroup := r.Group("/api")
 
@@ -77,55 +77,6 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Utilisateur créé avec succès",
 			"user":    result[0].Username,
-		})
-	})
-
-	r.POST("/api/login", func(c *gin.Context) {
-		var input struct {
-			Email    string `json:"email" binding:"required"`
-			Password string `json:"password" binding:"required"`
-		}
-
-		if err := c.ShouldBindJSON(&input); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Données manquantes"})
-			return
-		}
-
-		var users []types.User
-		_, err := client.From("users").Select("*", "exact", false).Eq("email", input.Email).ExecuteTo(&users)
-
-		if err != nil || len(users) == 0 {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Email inconnu"})
-			return
-		}
-
-		dbUser := users[0]
-
-		err = bcrypt.CompareHashAndPassword([]byte(dbUser.Password), []byte(input.Password))
-
-		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Mot de passe incorrect"})
-			return
-		}
-
-		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-			"user_id":  dbUser.ID,
-			"username": dbUser.Username,
-			"email":    dbUser.Email,
-			"exp":      time.Now().Add(time.Hour * 72).Unix(),
-		})
-
-		tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Erreur lors de la signature"})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Connexion réussie",
-			"token":   tokenString,
-			"user":    dbUser.Username,
 		})
 	})
 
