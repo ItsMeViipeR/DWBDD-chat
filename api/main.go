8ba76fb44941aacb9b4d0432a0b2d0a8517a452f
ItsMeViipeR <itsmeviiper@gmail.com>
2026-04-21 18:07:29 +0200

    feat: add RegisterHandler

--- api/main.go
+++ api/main.go
@@ -9,14 +9,7 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/supabase-community/postgrest-go"
-	"golang.org/x/crypto/bcrypt"
 )
-
-type RegisterInput struct {
-	Name     string `json:"name" binding:"required"`
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=6"`
-}
 
 func main() {
 	initDB()
@@ -32,6 +25,7 @@ func main() {
 		MaxAge:           12 * time.Hour,
 	}))
 
+	r.POST("/api/register", RegisterHandler)
 	r.POST("/api/login", LoginHandler)
 
 	authGroup := r.Group("/api")
@@ -43,42 +37,6 @@ func main() {
 		authGroup.POST("/change_email", CreateEmailHandler)
 		authGroup.POST("/topics", CreateTopicHandler)
 	}
-
-	r.POST("/api/register", func(c *gin.Context) {
-		var input RegisterInput
-
-		if err := c.ShouldBindJSON(&input); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		}
-
-		hash, err := bcrypt.GenerateFromPassword([]byte(input.Password), 0)
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-
-		newUser := types.User{
-			Username: input.Name,
-			Email:    input.Email,
-			Password: string(hash),
-		}
-
-		var result []types.User
-
-		_, err = client.From("users").Insert(newUser, false, "", "", "").ExecuteTo(&result)
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Erreur lors de la sauvegarde"})
-			fmt.Println("Erreur lors de la sauvegarde", err)
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Utilisateur créé avec succès",
-			"user":    result[0].Username,
-		})
-	})
 
 	r.GET("/api/messages", func(c *gin.Context) {
 		var input types.GetMessagesInput
